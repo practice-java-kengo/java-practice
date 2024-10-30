754a93b8cfed1bba150ce4b9a4730a30b6fd1fc3
k-masunari <k-masunari@esa-create.co.jp>
2024-10-30 21:53:35 +0900

    first comit

---
+++ src/javaPractice/Plactice_1.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class Plactice_1 {
+
+	public class HelloWorld {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, World !");
+	    }
+	}
+
+}
